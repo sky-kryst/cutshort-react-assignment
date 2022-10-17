c0db5f982213134627149db098b63f2dab92ed1a
sky-kryst <aakashmeshram@outlook.com>
2022-10-17 16:50:28 +0530

    feat(components): :sparkles: add form field to onboarding layout

--- components/OnboardingLayout/index.tsx
+++ components/OnboardingLayout/index.tsx
@@ -1,7 +1,22 @@
 interface IProps {
   children?: React.ReactNode;
+  submitButtonTitle: string;
 }
 
-export const OnboardingLayout: React.FC<IProps> = ({ children }) => {
-  return <div>{children}</div>;
+export const OnboardingLayout: React.FC<IProps> = ({
+  children,
+  submitButtonTitle,
+}) => {
+  return (
+    <div>
+      <div>
+        <form>
+          {children}
+          {submitButtonTitle && (
+            <button type="submit">{submitButtonTitle}</button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
