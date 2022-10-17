70339c9f3a6337516713b180a71e06597d65cde6
sky-kryst <aakashmeshram@outlook.com>
2022-10-17 13:45:11 +0530

    feat(pages): :sparkles: add onboarding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import Head from "next/head";
-import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/onboarding/1");
+  }, [router]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +19,6 @@ const Home: NextPage = () => {
       </Head>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
