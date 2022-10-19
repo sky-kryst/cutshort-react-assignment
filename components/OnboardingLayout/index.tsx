d3f9aca561a61b1710efd5a11df7b9cbd4233c6e
sky-kryst <aakashmeshram@outlook.com>
2022-10-19 18:19:11 +0530

    feat(components): :sparkles: add eden logo

--- components/OnboardingLayout/index.tsx
+++ components/OnboardingLayout/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Stepper } from "../Stepper";
+import Image from "next/image";
+import EdenLogo from "../../public/images/eden_logo.png";
 
 interface IProps {
   children?: React.ReactNode;
@@ -22,6 +24,7 @@ export const OnboardingLayout: React.FC<IProps> = ({
         className={`bg-neutral-50 h-full w-full flex flex-col items-center justify-between py-32`}
       >
         {/* <div className="w-1/3 border border-black"> */}
+        <Image src={EdenLogo} alt="Eden logo" height={39.75} width={94.5} />
         <Stepper noOfSteps={4} stepNo={pageNo} />
         <div className="flex flex-col justify-between items-center h-16">
           <h1 className="font-bold text-2xl">{title}</h1>
